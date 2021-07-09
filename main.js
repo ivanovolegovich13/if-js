477c55b59c15bedc56233a4aab57211498196ce2
ivanovolegovich13 <ivanovolegovich13@gmail.com>
2021-07-09 14:59:59 +0300

    lesson-1

---
+++ main.js
@@ -0,0 +1,20 @@
+var user = 'John Doe';
+console.log('user', user);
+
+var student = 'Anton';
+console.log('student', student);
+
+var user = student;
+// user = Anton
+console.log('user =', user);
+
+var test = 1 ;
+test = ++test;
+sum = test + '1';
+// sum = 21
+console.log('sum', sum);
+
+test = --test;
+sum = test + '1';
+// sum = 11
+console.log('sum', sum);
