1fcfa98a3e88cd1f62d2eda34d97c652103b5805
ivanovolegovich13 <ivanovolegovich13@gmail.com>
2021-07-26 12:39:43 +0300

    lesson-2.2 edited

--- lesson-2.2/main.js
+++ lesson-2.2/main.js
@@ -1,6 +1,6 @@
-var result = 1;
-var arr1 = [2, 3, 5, 8];
-for (var i=0; i < arr1.length; i++) {
+let result = 1;
+let arr1 = [2, 3, 5, 8];
+for (let i=0; i < arr1.length; i++) {
   result = result * arr1[i];
 }
 console.log(result);
@@ -8,16 +8,16 @@ console.log(result);
 
 
 
-var arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (var i=0; i < arr2.length; i++) {
+let arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i=0; i < arr2.length; i++) {
   if(arr2[i] > 5 && arr2[i] < 10) {
     console.log(arr2[i]);
   }
 }
 // 8, 6 //
 
-var arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (var i = 0; i < arr3.length; i++) {
+let arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < arr3.length; i++) {
   if ((arr3[i] % 2) === 0) {
     console.log(arr3[i]);
   }
